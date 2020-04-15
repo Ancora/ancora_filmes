a1b1ffb13ee2ef617b1c8f0406c8c9978b886740
Ancora <anderson.rocha@ancora-ti.com.br>
2020-04-15 12:24:55 -0300

    244-Login: Widget AppText.

--- lib/login/login_page.dart
+++ lib/login/login_page.dart
@@ -1,4 +1,6 @@
+import 'package:ancorafilmes/utils/validators.dart';
 import 'package:ancorafilmes/widgets/bg_login.dart';
+import 'package:ancorafilmes/widgets/text_field.dart';
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatefulWidget {
@@ -7,6 +9,8 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,8 +18,80 @@ class _LoginPageState extends State<LoginPage> {
         fit: StackFit.expand,
         children: <Widget>[
           BgLogin(),
-          //_body(),
+          _body(),
         ],
+      ),
+    );
+  }
+
+  _body() {
+    return Form(
+      key: _formKey,
+      child: Container(
+        padding: EdgeInsets.all(16),
+        child: ListView(
+          children: <Widget>[
+            Container(
+              padding: EdgeInsets.all(32),
+              child: Image.asset("assets/images/logoFilmes.png",
+                  width: 140, height: 140),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 16),
+              child: AppText(
+                "Login",
+                "Digite o seu login",
+                validator: (text) {
+                  return validateRequired(text, "Informe o login");
+                },
+                /* onSaved: (value) => this._input.login = value, */
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 16),
+              child: AppText(
+                "Senha",
+                "Digite a sua senha",
+                password: true,
+                validator: (text) {
+                  return validateRequired(text, "Informe a senha");
+                },
+                /* onSaved: (value) => this._input.senha = value, */
+              ),
+            ),
+            /* StreamBuilder<bool>(
+              stream: _bloc.progress.stream,
+              initialData: false,
+              builder: (context, snapshot) {
+                return Container(
+                  margin: EdgeInsets.only(top: 16),
+                  child: AppButton(
+                    "Login",
+                    _onClickLogin,
+                    showProgress: snapshot.data,
+                  ),
+                );
+              },
+            ), */
+            Container(
+              margin: EdgeInsets.only(top: 16),
+              /* child: GoogleSignInButton(
+                onPressed: _onClickGoogle,
+                borderRadius: 22,
+              ), */
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 0),
+              /* child: Center(
+                child: AppLink(
+                  "Cadastre-se",
+                  _onClickCadastro,
+                  color: Colors.white,
+                ),
+              ), */
+            ),
+          ],
+        ),
       ),
     );
   }
