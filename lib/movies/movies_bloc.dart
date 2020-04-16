c599be24dcde49b0f14b21ba95b6e7cadc032359
Ancora <anderson.rocha@ancora-ti.com.br>
2020-04-15 22:33:35 -0300

    254-SimpleBloc.

--- lib/movies/movies_bloc.dart
+++ lib/movies/movies_bloc.dart
@@ -1,25 +1,17 @@
-import 'package:ancorafilmes/movies/movie.dart';
 import 'package:ancorafilmes/movies/movie_api.dart';
-import 'package:rxdart/rxdart.dart';
+import 'package:ancorafilmes/utils/simple_bloc.dart';
 
-class MoviesBloc {
-  final _moviesController = BehaviorSubject<List<Movie>>();
-  get stream => _moviesController.stream;
-
+class MoviesBloc extends SimpleBloc {
   Future fetch({bool isRefresh = false}) async {
     try {
       if (isRefresh) {
-        _moviesController.sink.add(null);
+        add(null);
       }
       final movies = await MoviesApi.getMovies();
-      _moviesController.sink.add(movies);
+      add(movies);
       return movies;
     } catch (error) {
-      _moviesController.addError('Nenhum filme!');
+      addError('Nenhum filme!');
     }
-  }
-
-  void dispose() {
-    _moviesController.close();
   }
 }
